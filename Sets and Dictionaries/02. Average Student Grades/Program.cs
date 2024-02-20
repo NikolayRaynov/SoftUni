c8c7bc9fffd2a84771607736ee9ef8efdc2fe5f8
Nikolay <niksa.nr@gmail.com>
2024-02-20 22:00:58 +0200

    Added some exercises with dictionaries

---
+++ Sets and Dictionaries/02. Average Student Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> data = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                decimal grade = decimal.Parse(tokens[1]);
+
+                if (!data.ContainsKey(name))
+                {
+                    data.Add(name, new List<decimal>() { grade });
+                }
+                else
+                {
+                    data[name].Add(grade);
+                }
+            }
+
+            foreach (var student in data)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries/08. SoftUni Party/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            HashSet<string> party = new HashSet<string>();
+            HashSet<string> toBePrinted = new HashSet<string>();
+            bool isBreak = false;
+
+            while (command != "END")
+            {
+                if (command == "PARTY")
+                {
+                    while (command != "END")
+                    {
+                        if (party.Contains(command))
+                        {
+                            party.Remove(command);
+                        }
+                        command = Console.ReadLine();
+                        if (command == "END")
+                        {
+                            isBreak = true;
+                        }
+                    }
+                    if (isBreak)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    party.Add(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(party.Count);
+            foreach (string person in party)
+            {
+                if (char.IsDigit(person[0]))
+                {
+                    Console.WriteLine(person);
+                }
+                else
+                {
+                    toBePrinted.Add(person);
+                }
+            }
+
+            foreach (string person in toBePrinted)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
